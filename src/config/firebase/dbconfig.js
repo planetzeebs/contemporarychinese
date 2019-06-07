2cab1df1246b8088774f503789776565d87ab70b
planetzeebs <zeeb.dennis@gmail.com>
2019-06-07 20:30:16 +0900

    Initial Flash Cards Completed

---
+++ src/config/firebase/dbconfig.js
@@ -0,0 +1,10 @@
+export const DB_CONFIG = {
+    // Your web app's Firebase configuration
+    apiKey: "AIzaSyBxMaqCNiCinNxvyuG4MoHSPlR7lDVoBz0",
+    authDomain: "course-in-contemporary-chinese.firebaseapp.com",
+    databaseURL: "https://course-in-contemporary-chinese.firebaseio.com",
+    projectId: "course-in-contemporary-chinese",
+    storageBucket: "course-in-contemporary-chinese.appspot.com",
+    messagingSenderId: "561675028853",
+    appId: "1:561675028853:web:e99c1f7c1c802cbc"
+}
